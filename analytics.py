6d0d80095058c4012c297be461816b947d695158
johntelforduk <jtelford1@gmail.com>
2020-06-02 11:27:05 +0100

    Method for Year Month.

--- analytics.py
+++ analytics.py
@@ -9,48 +9,51 @@ class Analytics:
     def __init__(self, filename: str, datetime_now, refund_window: int):
         raw = pd.read_csv(filename)
 
-        # Filter down to successful transactions only.
-        clean = raw[raw['Status'].isin(['success'])]
+        clean = raw[raw['Status'].isin(['success'])]                # Filter down to successful transactions only.
 
-        # Filter down to sales only.
-        sales_only = clean[clean['Kind'].isin(['sale'])]
+        ###############
+        # Sales
+        ###############
+        sales_only = clean[clean['Kind'].isin(['sale'])]            # Filter down to Sales only.
 
         # Discard sales in the last <refund_window> days, because those sales haven't had time to have refunds yet.
         fortnight_ago = datetime.strftime((datetime_now + timedelta(days=-refund_window)),  "%Y-%m-%d")
         sales = sales_only[sales_only['Created At'] < fortnight_ago].copy()
 
-        year_month = []
+        # Add column Year Month.
+        ym_list = []
         for index, row in sales.iterrows():
+            ym_list.append(self.year_month(date_str=row['Created At'][0:10], delta_days=0))
+        sales['Year Month'] = ym_list
 
-            # Get the date part of the Created At string alone (discard time).
-            dts = row['Created At'][0:10]
-            created_at = datetime.strptime(dts, "%Y-%m-%d")
-
-            ym = str(created_at.year) + '-' + str(created_at.month).zfill(2)
-            year_month.append(ym)
-        sales['Year Month'] = year_month
-
+        # Aggregate sales by Year Month dimension.
         monthly_sales = sales.groupby('Year Month')['Amount'].sum().reset_index(name='Sales')
 
         ###############
-        refunds = clean[clean['Kind'].isin(['refund'])].copy()
+        # Refunds
+        ###############
+        refunds = clean[clean['Kind'].isin(['refund'])].copy()      # Filter down to Refunds only.
 
-        year_month = []
+        # Add column Year Month, shifted by Refund Window number of days.
+        ym_list = []
         for index, row in refunds.iterrows():
+            ym_list.append(self.year_month(date_str=row['Created At'][0:10], delta_days=-refund_window))
+        refunds['Year Month'] = ym_list
 
-            # Get the date part of the Created At string alone (discard time).
-            dts = row['Created At'][0:10]
-            created_at = datetime.strptime(dts, "%Y-%m-%d")
-
-            # A refund today is considered to be related to a sale <refund_window> days ago.
-            fortnight_earlier = created_at + timedelta(days=-refund_window)
-            ym = str(fortnight_earlier.year) + '-' + str(fortnight_earlier.month).zfill(2)
-            year_month.append(ym)
-        refunds['Year Month'] = year_month
-
+        # Aggregate refunds by Year Month dimension.
         monthly_refunds = refunds.groupby('Year Month')['Amount'].sum().reset_index(name='Refunds')
 
-        #############
+        ###############
+        # Refund Rate
+        ###############
         self.sales_and_refunds = pd.merge(monthly_sales, monthly_refunds, on='Year Month', how='outer')
         self.sales_and_refunds.fillna(0, inplace=True)
         self.sales_and_refunds['Return Rate'] = self.sales_and_refunds['Refunds'] / self.sales_and_refunds['Sales']
+
+    @staticmethod
+    def year_month(date_str: str, delta_days: int) -> str:
+        """For parm date string (yyyy-mm-dd), return it as a year-month string (yyyy-mm). The parm date will be moved
+        forward or backwards in time by the parm delta number of days."""
+        ym = datetime.strptime(date_str, "%Y-%m-%d")
+        adjusted_date = ym + timedelta(days=delta_days)
+        return str(adjusted_date.year) + '-' + str(adjusted_date.month).zfill(2)
